44f8336bfa25d3d13bff721e9c2d34f7fbe1257a
Dave Ogle <the_davester_808@hotmail.com>
2015-03-03 11:20:02 +0000

    Minor refactor of images and references

--- variations.php
+++ variations.php
@@ -5,8 +5,8 @@ include("Include/navigation.html");
 <div class ="mainContent">
     <H2>Variations</H2>
     <p>Most of the time Facade pattern is used in order to create a simpler interface for connecting to the application you are interested in.    
-    However there are some variations to use cases of Facade</p>
-      
+        However there are some variations to use cases of Facade</p>
+
     <div class="table-wrapper">
         <table>
             <tr>
@@ -18,8 +18,8 @@ include("Include/navigation.html");
                 <td><strong> Supplement:</strong></td>
                 <td>Other variation of Facade pattern is supplement of existing functions with new routines. This is more of a addition to traditional use of facade. 
                     Facade class still stores the subset of the class(number of methods)but it also contains some additional new functionality, usually based on the subset stored.
-                  <img src="Images/SupplementEx.png" alt="Suppliment Example" class="center" style="width: 60%; height: 60%; "/></td>
-                  
+                    <img src="Images/SupplementEx.png" alt="Suppliment Example" class="center"/></td>
+
             </tr>
             <tr>
                 <td><strong> Encapsulation:</strong></td>
@@ -28,40 +28,30 @@ include("Include/navigation.html");
                     Therefore the client MUST use Facade to access the system.
                     Encapsulating can be done for number of reasons:
                     <ul>
-                    <li> Track system usage</li>
-                    <p>Because all the access to system goes through Facade, the usage of the system can be easily tracked. If there was no Facade class, the data about usage would be distributed.</p>
+                        <li> Track system usage</li>
+                        <p>Because all the access to system goes through Facade, the usage of the system can be easily tracked. If there was no Facade class, the data about usage would be distributed.</p>
                     </ul>
                     <ul>
-                    <li> Swap out systems</li>
-                    <p>If you assume that your system will need to be changed at some point in the future, you should consider using Facade pattern. When your system is changed for a new one, the only class that need to be changed with the system is a Facade class,
-                        whereas when this design pattern is not used you would have to change all of the clients code. </p>
-                        <img src="Images/SystemSwapEx.png" alt="System Swap Example" class="center" style="width: 60%; height: 60%; "/>
+                        <li> Swap out systems</li>
+                        <p>If you assume that your system will need to be changed at some point in the future, you should consider using Facade pattern. When your system is changed for a new one, the only class that need to be changed with the system is a Facade class,
+                            whereas when this design pattern is not used you would have to change all of the clients code. </p>
+                        <img src="Images/SystemSwapEx.png" alt="System Swap Example" class="center"/>
                     </ul>
                 </td>
-                
+
             </tr>
             <tr>
                 <td><strong> Privacy:</strong></td>
                 <td> One of the main uses of facade pattern is hiding details that are not supposed to be revealed. Facade class only contains classes and variables that client can use hiding other ones.
-                As an example of use case: System has different clients that use system in different way. In this case to separate facade class are created for each client and contain only methods that this specific client can use. 
-                It only makes sense if client communicate with the system using Facade class and does not have other way to access a system.
-                <img src="Images/PrivacyExample.png" alt="Privacy Example" class="center" style="width: 60%; height: 60%; "/>             
-            </td>
+                    As an example of use case: System has different clients that use system in different way. In this case to separate facade class are created for each client and contain only methods that this specific client can use. 
+                    It only makes sense if client communicate with the system using Facade class and does not have other way to access a system.
+                    <img src="Images/PrivacyExample.png" alt="Privacy Example" class="center"/>             
+                </td>
             </tr>
-            
-            
-            
         </table>
-        
-        
- <h4>References</h4>
-    <h6>BISHOP, J. et al. (2007) <i>C# 3.0 Design Patterns: Use the Power of C# 3.0 to Solve Real-World Problems</i> O'Reilly Media</h6>
-    <h6>Best Practice Software Engineering (?) <i>Facade Pattern</i> [Online] http://best-practice-software-engineering.ifs.tuwien.ac.at/patterns/facade.html [Accessed: 1st March 2015]</h6>
-
-
-</div>
-<?php
-include("Include/footer.html");
-/*
+    </div>
+    <?php
+    include("Include/footer.html");
+    /*
  * End of file
  */
