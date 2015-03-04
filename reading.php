564c76d6604d6dff0a33dd2e67ed859f784d027e
Dave Ogle <the_davester_808@hotmail.com>
2015-03-04 17:38:52 +0000

    Add a video, remove the footer and add the links in as reading

--- reading.php
+++ reading.php
@@ -5,11 +5,18 @@ include("Include/navigation.html");
 <div class ="mainContent">
     <H2>Reading</H2>
     <p>Further reading</p>
-    
-    <p>https://www.youtube.com/watch?v=B1Y8fcYrz5o&list=PLF206E906175C7E07&index=14</p>
+    <iframe width="854" height="510" src="https://www.youtube.com/embed/B1Y8fcYrz5o?list=PLF206E906175C7E07" frameborder="0" allowfullscreen></iframe>
+
+    <h3>Reading:</h4>
+    <h4>Gamma, E. et al. (1995) <i>Design Patterns: Elements of Reusable Object-Oriented Software</i> Boston: Addison-Wesley</h4>
+    <h4>FREEMAN, E. et al. (2004) <i>Head First Design Patterns.</i> Sebastopol, Calif.: O'Reilly </h4>
+    <h4><a href="http://www.bradapp.com/docs/demeter-intro.html">APPLETON, B. <i>Introducing Demeter and its Laws</i></a></h4>
+    <h4><a href="https://pragprog.com/articles/tell-dont-ask">The Pragmatic Bookshelf (?) <i>Tell, Don't Ask</i></a></h4>
+    <h4>BISHOP, J. et al. (2007) <i>C# 3.0 Design Patterns: Use the Power of C# 3.0 to Solve Real-World Problems</i> O'Reilly Media</h4>
+    <h4><a href="http://best-practice-software-engineering.ifs.tuwien.ac.at/patterns/facade.html">Best Practice Software Engineering<i>Facade Pattern</i></a></h4>
+
 </div>
 <?php
-include("Include/footer.html");
 /*
  * End of file
  */
