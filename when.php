33f0adb83b82d78ad9ab7a7ef269f1de1a21fbfd
T Lennart <lennartta@gmail.com>
2015-03-04 10:55:04 +0000

    Image added, few changes made

--- when.php
+++ when.php
@@ -3,14 +3,13 @@ include("Include/header.html");
 include("Include/navigation.html");
 ?>
 <div class ="mainContent">
-    <h2>When should I use the Facade Design Pattern?</h2>
+    <h2><i>When should I use the Facade Design Pattern?</i></h2>
     
     <h3>A simple interface in a complex system</h3>
     <p>The facade pattern should be used in situations where you would prefer to have a simpler single interface 
         rather than utilising a more complex subsystem.</p>
 
-    <p>For example, in web services, one web service might provide access to a number of smaller services that have 
-    been hidden from the caller by the facade. It can be applied when a lot of complex classes are used to make a system simpler for users, as well as when only one class is used, to avoid using it directly. </p>
+    <p>For example, in web services, one web service might provide access to a number of smaller services that are hidden from the caller by the facade. It can be applied when a lot of complex classes are used to make a system simpler for users, as well as when only one class is used, to avoid using it directly. </p>
     </br>
 
     <h3>Duplications</h3>
@@ -18,19 +17,21 @@ include("Include/navigation.html");
         create a lead controller that will handle all of the repeating code, helping to reduce code duplication and 
         improving its maintainability. As it is much easier to change code once, than doing it multiple times in 
         several places.</p>
-    <p>The client will then, we will just call the lead controller to perform actions based on the parameters 
+    <p>The client will just call the lead controller to perform actions based on the parameters 
         provided. </p>
     </br>
 
     <h3>System changes frequently</h3>
     <p>Facade accesses the subsystems for the client, so if they change often, it does not affect the client, as facade deals with it. </p>
-     </br>
-
+    <img src ="Images/facade_adv.jpg" alt ="System changes">
+    
+    </br>
+     
     <h3>Facade vs Adapter patterns</h3>
     <p>The Adapter Pattern and the Facade Pattern solve different problems. The intent of the Adapter Pattern is to adapt one classes interface into an expected interface used by an existing client class or classes. The intent of the Facade Pattern is to simplify the API of a subsystem. </p>
      </br>
 
-     <h3>Simple example (not sure if it is needed)</h3>
+     <h3>Simple example</h3>
     <p>Let's say that you have been given a task to plan your friend's marriage. If you do everything on your own, 
         then imagine the things you need to cover. It will create a higher possibility for error, and increase the 
         chance of missing something that can drastically affect your friend's wedding. </p>
