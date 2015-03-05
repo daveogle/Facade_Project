274f2c5a91cba2210b5765cb9337d4cc54f0935c
Andrew Pond <a.r.pond@dundee.ac.uk>
2015-03-05 15:08:42 +0000

    Further Reading edits

--- reading.php
+++ reading.php
@@ -3,17 +3,16 @@ include("Include/header.html");
 include("Include/navigation.html");
 ?>
 <div class ="mainContent">
-    <H2>Reading</H2>
-    <p>Further reading</p>
-    <iframe width="854" height="510" src="https://www.youtube.com/embed/B1Y8fcYrz5o?list=PLF206E906175C7E07" frameborder="0" allowfullscreen></iframe>
+    <H2>Further Reading/Videos</H2>
+    <iframe class="center" width="854" height="510" src="https://www.youtube.com/embed/B1Y8fcYrz5o?list=PLF206E906175C7E07" frameborder="0" allowfullscreen></iframe>
 
     <h3>Reading:</h4>
-    <h4>Gamma, E. et al. (1995) <i>Design Patterns: Elements of Reusable Object-Oriented Software</i> Boston: Addison-Wesley</h4>
-    <h4>FREEMAN, E. et al. (2004) <i>Head First Design Patterns.</i> Sebastopol, Calif.: O'Reilly </h4>
-    <h4><a href="http://www.bradapp.com/docs/demeter-intro.html">APPLETON, B. <i>Introducing Demeter and its Laws</i></a></h4>
-    <h4><a href="https://pragprog.com/articles/tell-dont-ask">The Pragmatic Bookshelf (?) <i>Tell, Don't Ask</i></a></h4>
-    <h4>BISHOP, J. et al. (2007) <i>C# 3.0 Design Patterns: Use the Power of C# 3.0 to Solve Real-World Problems</i> O'Reilly Media</h4>
-    <h4><a href="http://best-practice-software-engineering.ifs.tuwien.ac.at/patterns/facade.html">Best Practice Software Engineering<i>Facade Pattern</i></a></h4>
+    <h4><i>Design Patterns: Elements of Reusable Object-Oriented Software</i> - Gamma, E. et al. (1995)</h4>
+    <h4><i>Head First Design Patterns.</i> - FREEMAN, E. et al. (2004) </h4>
+    <h4><a href="http://www.bradapp.com/docs/demeter-intro.html"><i>Introducing Demeter and its Laws</i> - APPLETON, B. </a></h4>
+    <h4><a href="https://pragprog.com/articles/tell-dont-ask"><i>Tell, Don't Ask</i> - The Pragmatic Bookshelf </a></h4>
+    <h4><i>C# 3.0 Design Patterns: Use the Power of C# 3.0 to Solve Real-World Problems</i> - BISHOP, J. et al. (2007)  </h4>
+    <h4><a href="http://best-practice-software-engineering.ifs.tuwien.ac.at/patterns/facade.html"><i>Facade Pattern</i> - Best Practice Software Engineering</a></h4>
 
 </div>
 <?php
